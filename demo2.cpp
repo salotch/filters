66f47ad95166cd859910c01485c67cf99fdb7a46
yara2 <yara200359@gmail.com>
2023-10-10 23:43:31 +0300

    edit

--- demo2.cpp
+++ demo2.cpp
@@ -1,11 +1,10 @@
 // FCAI – OOP Programming – 2023 - Assignment 1
-// Program Name:				filters.cpp
-// Last Modification Date:	/10/2023
-// Author1 and ID and Group:
-// Author2 and ID and Group:
-// Author3 and ID and Group:
-// Teaching Assistant:
-// Purpose:..........
+// Program : demo2.cpp
+// Last Modification Date:	10/10/2023
+// Yara Hazem Hassan Hussien and NO ID (Transfered from another university).
+// Salma Abd_Al_samie Ahmed Abd_Al_moaety and No ID (Transfered from another university).
+// Youssef Ali  and No ID (Transfered from another university).
+// Purpose: Making it easier for the user to modify images using any of the filters.
 
 #include <iostream>
 #include <fstream>
@@ -27,7 +26,6 @@ void black_white();
 void invert_filter();
 void rotate_90();
 void rotate_180();
-void rotate_270();
 void  saveImage1();
 void flip_image_horizontally();
 void flip_image_vertically();
@@ -41,21 +39,22 @@ int main()
   if(x=="0")
       return 0;
   else if(x=="6")
-  {if(n==90||n==270)
-      { saveImage1();
-          return 0;}
+  {
+      if(n==90||n==270)
+      {
+          saveImage1();
+          return 0;
+      }
   }
     saveImage();
     return 0;
-  saveImage();
-  return 0;
 }
 
-//_________________________________________
-void loadImage () {
+//Make user enter photo that he want to modify it.
+void loadImage ()
+{
    char imageFileName[100];
 
-   // Get gray scale image file name
    cout << "Enter the source image file name: ";
    cin >> imageFileName;
 
@@ -63,10 +62,11 @@ void loadImage () {
    strcat (imageFileName, ".bmp");
    readGSBMP(imageFileName, image);
 }
-void loadImage1 () {
+// To use it in merge filter allow user to enter second photo.
+void loadImage1 ()
+{
     char imageFileName[100];
 
-    // Get gray scale image file name
     cout << "Enter the source image file name: ";
     cin >> imageFileName;
 
@@ -74,11 +74,11 @@ void loadImage1 () {
     strcat (imageFileName, ".bmp");
     readGSBMP(imageFileName, imagem);
 }
-//_________________________________________
-void saveImage () {
+// Save image in another file after modification .
+void saveImage ()
+{
    char imageFileName[100];
 
-   // Get gray scale image target file name
    cout << "Enter the target image file name: ";
    cin >> imageFileName;
 
@@ -86,10 +86,11 @@ void saveImage () {
    strcat (imageFileName, ".bmp");
    writeGSBMP(imageFileName, image);
 }
-void saveImage1 () {
+// We use this function in rotate filter to save image after rotation.
+void saveImage1 ()
+{
     char imageFileName[100];
 
-    // Get gray scale image target file name
     cout << "Enter the target image file name: ";
     cin >> imageFileName;
 
@@ -98,17 +99,15 @@ void saveImage1 () {
     writeGSBMP(imageFileName, image1);
 }
 
-//_________________________________________
-void doSomethingForImage() {
+// This function we called it in main to make user select filter that he wanted and call the function of the filter.
+void doSomethingForImage()
+{
     cout<<"Please select a filter to apply or 0 to exit: \n";
     cout<<"1-Black & White Filter\n"<<"2-Invert Filter\n"<<"3-Merge Filter\n";
     cout<<"4-Flip Image\n"<<"5-Darken and Lighten Image\n"<<"6-Rotate Image\n";
-    cout<<"7-Detect Image Edges\n"<<"8-Enlarge Image\n"<<"9-Shrink Image\n";
-    cout<<"a-Mirror 1/2 Image\n"<<"b-Shuffle Image\n"<<"c-Blur Image\n";
-    cout<<"d-Crop Image\n"<<"e-Skew Image Right\n"<<"f-Skew Image Up\n";
     cout<<"s-Save the image to a file\n"<<"0-Exit\n";
 
-    cin>>x;
+    cin>>x; // this variable that make user select no. of filter that he wanted
     if(x=="1")
         black_white();
     else if(x=="2")
@@ -121,7 +120,7 @@ void doSomethingForImage() {
     {
         cout << "Do you want to flip (h)orizontally or (v)ertically ?"<<endl;
         char f;
-        cin >> f;
+        cin >> f;// make user select h or v
         if (f=='h')
             flip_image_horizontally();
         else if (f=='v')
@@ -134,12 +133,12 @@ void doSomethingForImage() {
     else if(x=="6")
     {
         cout<<"Rotate (90), (180) or (270) degrees? ";
-        cin>>n;
+        cin>>n; // user select the degree of rotate that he want to happen in photo.
         if(n==90)
             rotate_90();
         else if(n==180)
             rotate_180();
-        else if(n==270)
+        else if(n==270) // we call function rotate 180 and rotate 90.
         {
             rotate_180();
             rotate_90();
@@ -148,10 +147,13 @@ void doSomethingForImage() {
 
 
 }
+// this function that turn the photo to black & white.
 void black_white()
 {
-    for (int i = 0; i < SIZE; i++) {
-        for (int j = 0; j < SIZE; j++) {
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
             if (image[i][j] > 127)
                 image[i][j] = 255;
             else
@@ -159,6 +161,7 @@ void black_white()
         }
     }
 }
+// this function that invert the photo .
 void invert_filter()
 {
     for (int i = 0; i < SIZE; i++)
@@ -169,6 +172,7 @@ void invert_filter()
         }
     }
 }
+// this function flip the image horizontally.
 void flip_image_horizontally()
 {
     int temp =0;
@@ -182,6 +186,7 @@ void flip_image_horizontally()
         }
     }
 }
+// this function flip the image vertically.
 void flip_image_vertically()
 {
     int temp =0;
@@ -196,17 +201,25 @@ void flip_image_vertically()
     }
 
 }
+// this filter rotate photo 90 degree.
 void rotate_90()
-{ for (int i = 255; i >=0; i--) {
-        for (int j = 0; j< SIZE; j++) {
+{
+    for (int i = 255; i >=0; i--)
+    {
+        for (int j = 0; j< SIZE; j++)
+        {
             image1[j][i]=image[255-i][j];
         }
     }
 
 }
+// this filter rotate photo 180 degree.
 void rotate_180()
-{ for (int i = 0; i < SIZE/2; i++) {
-        for (int j = 0; j< SIZE; j++) {
+{
+    for (int i = 0; i < SIZE/2; i++)
+    {
+        for (int j = 0; j< SIZE; j++)
+        {
             int t=image[i][j];
             image[i][j]=image[255-i][255-j];
             image[255-i][255-j]=t;
@@ -214,12 +227,14 @@ void rotate_180()
     }
 
 }
+// this function contain two function one make photo lighter and other darker.
 void light_dark() {
     char x;
     cout<<"Do you want to (d)arken or (l)ighten ?";
-    cin>>x;
-    if (x== 'l')
-    {for (int i = 0; i < SIZE; i++)
+    cin>>x; //this variable make user select what he wants to do in photo .
+    if (x== 'l')// make photo lighter
+    {
+        for (int i = 0; i < SIZE; i++)
         {
             for (int j = 0; j < SIZE; j++)
             {int x=255-image [i][j];
@@ -228,8 +243,9 @@ void light_dark() {
             }
         }
     }
-    else if (x== 'd' )
-    {for (int i = 0; i < SIZE; i++)
+    else if (x== 'd' )//make photo darker.
+    {
+        for (int i = 0; i < SIZE; i++)
         {
             for (int j = 0; j < SIZE; j++)
             {
@@ -239,12 +255,16 @@ void light_dark() {
         }
     }
 }
-void merge(){
+//this function merge to photo together.
+void merge()
+{
 
     loadImage1();
 
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
+        for (int i = 0; i < SIZE; i++)
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
                 image[i][j] = image[i][j] / 2 + imagem[i][j] / 2;
             }
 
